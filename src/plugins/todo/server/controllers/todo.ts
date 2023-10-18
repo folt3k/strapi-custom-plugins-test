4394bcfe326d6a709c18973c709d5a5e5b777751
folt3k <folt3k@gmail.com>
2023-10-18 16:11:45 +0200

    some methods

--- src/plugins/todo/server/controllers/todo.ts
+++ src/plugins/todo/server/controllers/todo.ts
@@ -5,7 +5,43 @@
 import { Strapi } from "@strapi/strapi";
 
 export default ({ strapi }: { strapi: Strapi }) => ({
-  find(ctx) {
-    return strapi.plugin("todo").service("todo").find(ctx.query);
+  async find(ctx) {
+    try {
+      return await strapi.plugin("todo").service("todo").find(ctx.query);
+    } catch (err) {
+      ctx.throw(500, err);
+    }
+  },
+
+  async delete(ctx) {
+    try {
+      ctx.body = await strapi.plugin("todo").service("todo").delete(ctx.params.id);
+    } catch (err) {
+      ctx.throw(500, err);
+    }
+  },
+
+  async create(ctx) {
+    try {
+      ctx.body = await strapi.plugin("todo").service("todo").create(ctx.request.body);
+    } catch (err) {
+      ctx.throw(500, err);
+    }
+  },
+
+  async update(ctx) {
+    try {
+      ctx.body = await strapi.plugin("todo").service("todo").update(ctx.params.id, ctx.request.body);
+    } catch (err) {
+      ctx.throw(500, err);
+    }
+  },
+
+  async toggle(ctx) {
+    try {
+      ctx.body = await strapi.plugin("todo").service("todo").toggle(ctx.params.id);
+    } catch (err) {
+      ctx.throw(500, err);
+    }
   },
 });
--- src/plugins/todo/server/routes/todo.ts
+++ src/plugins/todo/server/routes/todo.ts
@@ -10,5 +10,44 @@ export default {
         auth: false,
       },
     },
+    {
+      method: "POST",
+      path: "/create",
+      handler: "todo.create",
+      config: {
+        policies: [],
+        auth: false,
+      },
+    },
+
+    {
+      method: "DELETE",
+      path: "/delete/:id",
+      handler: "todo.delete",
+      config: {
+        policies: [],
+        auth: false,
+      },
+    },
+
+    {
+      method: "PUT",
+      path: "/toggle/:id",
+      handler: "todo.toggle",
+      config: {
+        policies: [],
+        auth: false,
+      },
+    },
+
+    {
+      method: "PUT",
+      path: "/update/:id",
+      handler: "todo.update",
+      config: {
+        policies: [],
+        auth: false,
+      },
+    },
   ],
 };
